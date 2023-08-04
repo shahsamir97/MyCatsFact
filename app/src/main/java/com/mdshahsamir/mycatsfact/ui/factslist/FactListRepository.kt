95c0b5878d26b1b441a231b9374f51e20ca850e5
shahsamir <mdshahsamir@gmail.com>
2023-08-04 12:56:01 +0600

    redundant imports removed

--- app/src/main/java/com/mdshahsamir/mycatsfact/ui/factslist/FactListRepository.kt
+++ app/src/main/java/com/mdshahsamir/mycatsfact/ui/factslist/FactListRepository.kt
@@ -1,15 +1,10 @@
 package com.mdshahsamir.mycatsfact.ui.factslist
 
-import android.net.ConnectivityManager
 import com.mdshahsamir.mycatsfact.database.CatDao
 import com.mdshahsamir.mycatsfact.model.Cat
-import com.mdshahsamir.mycatsfact.model.Fact
-import com.mdshahsamir.mycatsfact.networking.CatApiService
 import com.mdshahsamir.mycatsfact.networking.RemoteDataSource
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.withContext
-import java.lang.Exception
 
 interface FactListRepository {
     suspend fun getCatFacts(cats: List<Cat>)
@@ -28,7 +23,7 @@ class FactListRepositoryImpl(
             try {
                 cats.forEach {
                     val fact = remoteDataSource.fetchFact()
-                    if (fact.isSuccess){
+                    if (fact.isSuccess) {
                         it.fact = fact.getOrThrow().fact
                     }
                 }
